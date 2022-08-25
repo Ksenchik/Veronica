cac913a22c75f333dfcd8b1ad0dcfe7e53fd710d
Ksenchik <oksana.100@mail.ru>
2022-08-25 15:08:02 +0300

    fixed: burger menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-var toggleButton = document.querySelector('.toggle-menu');
-var navBar = document.querySelector('.nav-bar');
-toggleButton.addEventListener('click', function () {
-	navBar.classList.toggle('toggle');
+let header__burger = document.querySelectorAll('.header__burger,.header__link');
+let header_menu = document.querySelector('.header__menu');
+let back = document.querySelector('body');
+
+header__burger.forEach(function (item) {
+   item.onclick = function () {
+      item.classList.toggle('active');
+      header_menu.classList.toggle('active');
+      back.classList.toggle('lock');
+   }
 });
